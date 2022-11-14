6324168e7cf5ec6b4caad9fc3510a25d0e83a5e7
MGicas <89281701+MGicas@users.noreply.github.com>
2022-11-14 03:35:46 -0500

    AdminCommand

--- Stl_Service/src/edu/uco/stl/service/usecase/command/implementation/CreateAdminCommandImpl.java
+++ Stl_Service/src/edu/uco/stl/service/usecase/command/implementation/CreateAdminCommandImpl.java
@@ -1,5 +1,7 @@
 package edu.uco.stl.service.usecase.command.implementation;
 
+import edu.uco.stl.crosscutting.exception.crosscutting.UseCaseCustomException;
+import edu.uco.stl.crosscutting.execption.StlCustomException;
 import edu.uco.stl.data.daofactory.DAOFactory;
 import edu.uco.stl.data.enumeration.DAOFactoryType;
 import edu.uco.stl.domain.AdminDTO;
@@ -14,7 +16,22 @@ public class CreateAdminCommandImpl implements CreateAdminCommand{
 
 	@Override
 	public void execute(AdminDTO admin) {
-		
+		try {
+			factory.initTransction();
+			useCase.execute(admin);
+			factory.confirmTransaction();
+		}catch (UseCaseCustomException exception) {
+			factory.cancelTransaction();
+			throw exception;
+		}catch (StlCustomException exception) {
+			factory.cancelTransaction();
+			throw UseCaseCustomException.wrapException(null, exception);
+		}catch (Exception exception) {
+			factory.cancelTransaction();
+			throw UseCaseCustomException.CreateBusinessException(null, exception);
+		}finally {
+			factory.closeTransaction();
+		}
 	}
 
 }
